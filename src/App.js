f7f4ccb208acd10c4c1a077aca48cad5a04a6fea
letocollection <leonard@letocollection.com>
2016-11-11 08:40:05 -0800

    started form

--- src/App.js
+++ src/App.js
@@ -5,15 +5,28 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="jumbotron col-sm-6 col-sm-offset-3 text-center">
+      <h1>Search Article</h1>
+      <div className="col-sm-12">
+        <form>
+          <div className="form-group">
+            <input
+              className="form-control"
+              placeholder="Articles"
+              type="text" />
+          </div>
+          <div className="form-group col-sm-4 col-sm-offset-4">
+            <button
+              className="btn btn-block btn-success"
+              type="submit">
+                Continue
+            </button>
+          </div>
+
+
+        </form>
       </div>
+    </div>
     );
   }
 }
