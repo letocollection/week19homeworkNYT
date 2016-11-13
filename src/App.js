0a8bd611fb17d6b652b7df9bd129e61018a8ef19
letocollection <leonard@letocollection.com>
2016-11-12 18:13:33 -0800

    homework submit

--- src/App.js
+++ src/App.js
@@ -8,23 +8,48 @@ class App extends Component {
       <div className="jumbotron col-sm-6 col-sm-offset-3 text-center">
       <h1>Search Article</h1>
       <div className="col-sm-12">
-        <form>
-          <div className="form-group">
-            <input
-              className="form-control"
-              placeholder="Articles"
-              type="text" />
-          </div>
-          <div className="form-group col-sm-4 col-sm-offset-4">
-            <button
-              className="btn btn-block btn-success"
-              type="submit">
-                Continue
-            </button>
-          </div>
+        <div class="panel panel-default">
+              <form>
+                <div className="form-group">
+                  <input
+                    className="form-control"
+                    placeholder="Articles"
+                    type="text" />
+                </div>
+
+                <div class="form-group">
+      							<label>Number of Records to Retrieve: </label>
+      						  <select class="form-control" id="search-number">
+      							<option>1</option>
+      							<option>5</option>
+      							<option>10</option>
+      							</select>
+      					</div>
+
+                <div className="form-group">
+                  <input
+                    className="form-control"
+                    placeholder="Articles"
+                    type="text" />
+                </div>
+                  <div className="form-group">
+                    <input
+                      className="form-control"
+                      placeholder="Articles"
+                      type="text" />
+                  </div>
+
+                <div className="form-group col-sm-4 col-sm-offset-4">
+                  <button
+                    className="btn btn-block btn-success"
+                    type="submit">
+                      Continue
+                  </button>
+                </div>
 
 
-        </form>
+              </form>
+          </div>
       </div>
     </div>
     );
